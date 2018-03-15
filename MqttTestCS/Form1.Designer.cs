e52e0039de02d26b23fee88e2b2fe530f94d5a0e
Jesse Connell <jesseconnell@gmail.com>
2018-03-15 15:40:59 -0500

    Working pub/sub

---
+++ MqttTestCS/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace MqttTestCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.publishButton = new System.Windows.Forms.Button();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.topicsCheckListBox = new System.Windows.Forms.CheckedListBox();
+            this.firehoseButton = new System.Windows.Forms.Button();
+            this.clientIdTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.subscribeCustomButton = new System.Windows.Forms.Button();
+            this.customTopicTextBox = new System.Windows.Forms.TextBox();
+            this.unsubscribeCustomButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // publishButton
+            // 
+            this.publishButton.Location = new System.Drawing.Point(12, 153);
+            this.publishButton.Name = "publishButton";
+            this.publishButton.Size = new System.Drawing.Size(75, 23);
+            this.publishButton.TabIndex = 4;
+            this.publishButton.Text = "&Publish";
+            this.publishButton.UseVisualStyleBackColor = true;
+            this.publishButton.Click += new System.EventHandler(this.publishButton_Click);
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(93, 155);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(406, 20);
+            this.messageTextBox.TabIndex = 5;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(402, 12);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(97, 23);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.Text = "&Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logTextBox.Location = new System.Drawing.Point(12, 181);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.Size = new System.Drawing.Size(487, 280);
+            this.logTextBox.TabIndex = 7;
+            // 
+            // topicsCheckListBox
+            // 
+            this.topicsCheckListBox.FormattingEnabled = true;
+            this.topicsCheckListBox.Items.AddRange(new object[] {
+            "topic1",
+            "topic1/sub1",
+            "topic1/sub2",
+            "topic1/sub3",
+            "topic2",
+            "topic3",
+            "topic3/sub1"});
+            this.topicsCheckListBox.Location = new System.Drawing.Point(12, 38);
+            this.topicsCheckListBox.Name = "topicsCheckListBox";
+            this.topicsCheckListBox.Size = new System.Drawing.Size(123, 109);
+            this.topicsCheckListBox.TabIndex = 3;
+            this.topicsCheckListBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.topicsCheckListBox_ItemCheck);
+            // 
+            // firehoseButton
+            // 
+            this.firehoseButton.Location = new System.Drawing.Point(402, 41);
+            this.firehoseButton.Name = "firehoseButton";
+            this.firehoseButton.Size = new System.Drawing.Size(97, 23);
+            this.firehoseButton.TabIndex = 6;
+            this.firehoseButton.Text = "Start &Firehose";
+            this.firehoseButton.UseVisualStyleBackColor = true;
+            this.firehoseButton.Click += new System.EventHandler(this.firehosePublishButton_Click);
+            // 
+            // clientIdTextBox
+            // 
+            this.clientIdTextBox.Location = new System.Drawing.Point(66, 12);
+            this.clientIdTextBox.Name = "clientIdTextBox";
+            this.clientIdTextBox.Size = new System.Drawing.Size(324, 20);
+            this.clientIdTextBox.TabIndex = 1;
+            this.clientIdTextBox.Text = "(auto)";
+            this.clientIdTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Client &Id:";
+            // 
+            // subscribeCustomButton
+            // 
+            this.subscribeCustomButton.Location = new System.Drawing.Point(13, 45);
+            this.subscribeCustomButton.Name = "subscribeCustomButton";
+            this.subscribeCustomButton.Size = new System.Drawing.Size(100, 23);
+            this.subscribeCustomButton.TabIndex = 8;
+            this.subscribeCustomButton.Text = "&Subscribe Custom";
+            this.subscribeCustomButton.UseVisualStyleBackColor = true;
+            this.subscribeCustomButton.Click += new System.EventHandler(this.subscribeCustomButton_Click);
+            // 
+            // customTopicTextBox
+            // 
+            this.customTopicTextBox.Location = new System.Drawing.Point(13, 19);
+            this.customTopicTextBox.Name = "customTopicTextBox";
+            this.customTopicTextBox.Size = new System.Drawing.Size(236, 20);
+            this.customTopicTextBox.TabIndex = 9;
+            this.customTopicTextBox.Text = "+/sub1";
+            // 
+            // unsubscribeCustomButton
+            // 
+            this.unsubscribeCustomButton.Location = new System.Drawing.Point(149, 45);
+            this.unsubscribeCustomButton.Name = "unsubscribeCustomButton";
+            this.unsubscribeCustomButton.Size = new System.Drawing.Size(100, 23);
+            this.unsubscribeCustomButton.TabIndex = 10;
+            this.unsubscribeCustomButton.Text = "&Unsubscribe";
+            this.unsubscribeCustomButton.UseVisualStyleBackColor = true;
+            this.unsubscribeCustomButton.Click += new System.EventHandler(this.unsubscribeCustomButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.subscribeCustomButton);
+            this.groupBox1.Controls.Add(this.customTopicTextBox);
+            this.groupBox1.Controls.Add(this.unsubscribeCustomButton);
+            this.groupBox1.Location = new System.Drawing.Point(141, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 78);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "&Custom Topic";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 473);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clientIdTextBox);
+            this.Controls.Add(this.firehoseButton);
+            this.Controls.Add(this.topicsCheckListBox);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.publishButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button publishButton;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.CheckedListBox topicsCheckListBox;
+        private System.Windows.Forms.Button firehoseButton;
+        private System.Windows.Forms.TextBox clientIdTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button subscribeCustomButton;
+        private System.Windows.Forms.TextBox customTopicTextBox;
+        private System.Windows.Forms.Button unsubscribeCustomButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ MqttTestCS/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MqttTestCS
+{
+    public partial class Form1 : Form
+    {
+        MqttClient client = new MqttClient("127.0.0.1", 1883, false, null, null, MqttSslProtocols.None);
+        Dictionary<ushort, string> pendingSubscribes = new Dictionary<ushort, string>();
+        Dictionary<ushort, string> pendingUnsubscribes = new Dictionary<ushort, string>();
+        bool callbackManagingCheckStates;
+        System.Threading.Thread firehoseThread;
+        bool firehoseOn = false;
+        bool connected;
+        bool closing;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            client.ConnectionClosed += Client_ConnectionClosed;
+            client.MqttMsgPublished += Client_MqttMsgPublished;
+            client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+            client.MqttMsgSubscribed += Client_MqttMsgSubscribed;
+            client.MqttMsgUnsubscribed += Client_MqttMsgUnsubscribed;
+            setConnected(false);
+
+            firehoseThread = new System.Threading.Thread(() =>
+            {
+                firehose();
+            });
+            firehoseThread.IsBackground = true;
+            firehoseThread.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (client.IsConnected)
+            {
+                stopFirehose();
+                closing = true;
+                firehoseThread.Join();
+                client.Disconnect();
+                while (connected)
+                {
+                    log("Waiting for disconnect...");
+                    System.Threading.Thread.Sleep(100);
+                }
+                log("DISCONNECT ok - goodbye!");
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+
+        void log(string fmt, params object[] args)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => {
+                    log(fmt, args);
+                }));
+                return;
+            }
+
+            string line = fmt + " (not yet formatted)";
+            try
+            {
+                line = string.Format(fmt, args);
+                logTextBox.AppendText(line + System.Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("Error logging '{0}'.  Exception: {1}", line, ex.Message);
+            }
+        }
+
+        void setConnected(bool b)
+        {
+            connected = b;
+            connectButton.Enabled = !b;
+            messageTextBox.Enabled = b;
+            publishButton.Enabled = b;
+            firehoseButton.Enabled = b;
+            topicsCheckListBox.Enabled = b;
+        }
+
+        private void Client_ConnectionClosed(object sender, EventArgs e)
+        {
+            setConnected(false);
+        }
+        private void Client_MqttMsgPublished(object sender, MqttMsgPublishedEventArgs e)
+        {
+            log("PUBLISH {0} msg {1}", e.IsPublished ? "ok" : "failed", e.MessageId);
+            Invoke(new Action(() =>
+            {
+                publishButton.Enabled = true;
+            }));
+        }
+        private void Client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                log("RECEIVE {0}:  {1}", e.Topic, Encoding.ASCII.GetString(e.Message));
+            }));
+        }
+        private void Client_MqttMsgSubscribed(object sender, MqttMsgSubscribedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                try
+                {
+                    string topic = pendingSubscribes[e.MessageId];
+                    pendingSubscribes.Remove(e.MessageId);
+                    log("SUBSCRIBE {0} ok", topic);
+                    int index = topicsCheckListBox.Items.IndexOf(topic);
+                    if(index != -1)
+                    { 
+                        callbackManagingCheckStates = true;
+                        topicsCheckListBox.SetItemChecked(index, true);
+                        topicsCheckListBox.Enabled = true;
+                        topicsCheckListBox.Select();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    log("Caught Exception {0}, message='{1}'", ex.ToString(), ex.Message);
+                }
+                callbackManagingCheckStates = false;
+            }));
+        }
+        private void Client_MqttMsgUnsubscribed(object sender, MqttMsgUnsubscribedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                try {
+
+                    string topic = pendingUnsubscribes[e.MessageId];
+                    pendingUnsubscribes.Remove(e.MessageId);
+                    log("UNSUBSCRIBE {0} ok", topic);
+                    int index = topicsCheckListBox.Items.IndexOf(topic);
+                    if (index != -1)
+                    {
+                        callbackManagingCheckStates = true;
+                        topicsCheckListBox.SetItemChecked(index, false);
+                        topicsCheckListBox.Enabled = true;
+                        topicsCheckListBox.Select();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    log("Caught Exception {0}, message='{1}'", ex.ToString(), ex.Message);
+                }
+                callbackManagingCheckStates = false;
+            }));
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            connectButton.Enabled = false;
+            try
+            {
+                string clientId = clientIdTextBox.Text == "(auto)"
+                    ? Guid.NewGuid().ToString()
+                    : clientIdTextBox.Text;
+
+                clientIdTextBox.Text = clientId;
+
+                byte b = client.Connect(clientId, "", "", true, MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true,
+                    //string.Format("/{0}/will", clientId), "my will",
+                    "/topic1", string.Format("{0} just died  :(", clientId),
+                    false, 600);
+
+                if (b == 0)
+                    log("CONNECT ok");
+                else
+                    log("CONNECT returned with error: {}", b);
+
+                setConnected(b == 0);
+            }
+            catch (Exception ex)
+            {
+                log("Caught Exception {0}, message='{1}'", ex.ToString(), ex.Message);
+            }
+        }
+
+        void publish(string topic, string message)
+        {
+            try
+            {
+                ushort msgId = client.Publish(topic, Encoding.ASCII.GetBytes(message), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+                log("PUBLISH topic: {0} msgId: {1} msg: {2}", topic, msgId, message);
+            }
+            catch (Exception ex)
+            {
+                log("Caught Exception {0}, message='{1}'", ex.ToString(), ex.Message);
+                topicsCheckListBox.Enabled = true;
+            }
+        }
+
+        string SelectedTopic
+        {
+            get {
+                return topicsCheckListBox.SelectedIndex == -1 
+                    ? null 
+                    : topicsCheckListBox.GetItemText(topicsCheckListBox.SelectedItem);
+            }
+        }
+
+        private void publishButton_Click(object sender, EventArgs e)
+        {
+            if (SelectedTopic == null)
+            {
+                log("No topic selected");
+                return;
+            }
+            publishButton.Enabled = false;
+            publish(SelectedTopic, messageTextBox.Text);
+            publishButton.Enabled = true;
+        }
+
+        private void topicsCheckListBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (callbackManagingCheckStates) return;
+            try
+            {
+                System.Console.WriteLine("ItemCheck() with sender = {0}, e.cur={1}, e.new={2}, e.idx={3}", sender.ToString(), e.CurrentValue, e.NewValue, e.Index);
+                topicsCheckListBox.Enabled = false;
+                string topic = topicsCheckListBox.GetItemText(topicsCheckListBox.Items[e.Index]);
+
+                bool subscribing = e.NewValue == CheckState.Checked;
+                log("{0} topic {1}", subscribing ? "SUBSCRIBE" : "UNSUBSCRIBE", topic);
+                e.NewValue = e.CurrentValue;  // Let subscribe/unsubscribe event manage this
+
+                if (subscribing)
+                {
+                    ushort msgId = client.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+                    pendingSubscribes[msgId] = topic;
+                }
+                else
+                {
+                    ushort msgId = client.Unsubscribe(new string[] { topic });
+                    pendingUnsubscribes[msgId] = topic;
+                }
+            }
+            catch (Exception ex)
+            {
+                log("Caught Exception {0}, message='{1}'", ex.ToString(), ex.Message);
+                topicsCheckListBox.Enabled = true;
+            }
+        }
+
+        void firehose()
+        {
+            System.Random r = new System.Random();
+
+            try
+            {
+                while (!closing)
+                {
+                    if (firehoseOn)
+                    {
+                        // publish
+                        int index = r.Next(topicsCheckListBox.Items.Count);
+                        string topic = topicsCheckListBox.GetItemText(topicsCheckListBox.Items[index]);
+                        publish(topic, r.NextDouble().ToString());
+                    }
+                    System.Threading.Thread.Sleep(1);
+                }
+            }
+            catch (Exception ex)
+            {
+                log("Caught Exception {0}, message='{1}'", ex.ToString(), ex.Message);
+            }
+        }
+
+        void startFirehose()
+        {
+            firehoseOn = true;
+            firehoseButton.Text = "Stop &Firehose";
+            publishButton.Enabled = false;
+        }
+
+        void stopFirehose()
+        {
+            firehoseOn = false;
+            firehoseButton.Text = "Start &Firehose";
+            publishButton.Enabled = true;
+        }
+
+        private void firehosePublishButton_Click(object sender, EventArgs e)
+        {
+            if (firehoseOn)
+                stopFirehose();
+            else
+                startFirehose();
+        }
+
+        private void subscribeCustomButton_Click(object sender, EventArgs e)
+        {
+            string topic = customTopicTextBox.Text;
+            ushort msgId = client.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            pendingSubscribes[msgId] = topic;
+        }
+
+        private void unsubscribeCustomButton_Click(object sender, EventArgs e)
+        {
+            string topic = customTopicTextBox.Text;
+            ushort msgId = client.Unsubscribe(new string[] { topic } );
+            pendingUnsubscribes[msgId] = topic;
+        }
+    }
+}
